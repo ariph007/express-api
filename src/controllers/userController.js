ddfa34e8976c7432dd658632ae6ddde216b772a1
Ari Prayoga Hutabarat <ariprayoga007@gmail.com>
2022-05-23 10:34:03 +0700

    Added routing to endpoint register and login

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,10 +4,82 @@ const jwt = require('jsonwebtoken')
 
 exports.register = async(req, res) => {
     try {
-        const {firstName, lastName, email, password} = req.body
-        console.log(req.body)
+        //* Request body dimasukkan dalam object
+        const {firstName, lastName, username, email, password} = req.body
+
+        //* Check field apakah ada yang kosong ? kala ada kasih pesan
+        if(firstName && lastName && username && email && password === ""){
+            return res.status(400).send({
+                message: 'field should not be empty'
+            })
+        }
+        
+        //* Field password dari user di encrypt
+        const hashedPassword = bcrypt.hashSync(password, 8)
+       
+        //* Masukkan data dari field yang telah diisi oleh user ke database menggunakan knex
+        let insertData = await knex('users').insert({
+            firstName: firstName,
+            lastName: lastName,
+            username: username,
+            email: email,
+            password: hashedPassword
+        }).then(insertedId => {
+            return insertedId
+        })
+
+        const token = jwt.sign({
+            id: insertData[0]
+        }, process.env.JWT_KEY, {expiresIn: 3600})
+
+        //* Beri status success ketika tidak ada eror
+        return res.status(200).send({
+            message: 'register success',
+            data: insertData, 
+            token: token
+        })
+
+        //* Kalau ada eror ketika insert ke database, beri pesan eror
     } catch (error) {
-        req.status(500).send({
+        res.status(500).send({
+            code: 500,
+            status: false,
+            message: error.message,
+            data: null
+        })
+    }
+}
+
+//* buat endpoint login
+exports.login = async(req, res) =>{
+    //* Jika tidak ada eror, select * from table user where username = req.body.username
+    try {
+        let getData = await knex('users').where({
+            username: req.body.username
+        }).select('*')
+
+    //* Cek apakah password valid ? caranya compare req.body.password && getData[0].password
+        const isPasswordValid = bcrypt.compareSync(req.body.password, getData[0].password)
+
+    //* Jika password tidak valid, maka kembalikan pesan 'password invalid
+        if(!isPasswordValid){
+            return res.status(400).send({
+                message: 'password invalid'
+            })
+        }
+
+        const token = jwt.sign({
+            id:getData[0].id
+        }, process.env.JWT_KEY, {expiresIn: 3600})
+
+        return res.status(200).send({
+            message: 'login succesfull',
+            data : getData,
+            token: token
+        })
+
+    } catch (error) {
+        res.status(500).send({
             code: 500,
             status: false,
             message: error.message,
--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 app.use(cors({ origin: true, credentials: true }));
 
-app.use('/', router);
+// app.use('/', router);
 app.use('/user', userRouter)
 
 app.listen(process.env.SERVER_PORT, () => {console.log('Server Running')});
